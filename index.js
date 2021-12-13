fc36d3b22154c25e2c356dc7eaa15d492ac0a10a
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-13 14:08:20 -0500

    added user questions to index.js

--- index.js
+++ index.js
@@ -4,7 +4,54 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the project title? ',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Describe your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'What is this project used for?',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'Select a license for this project.',
+        name: 'license',
+        choices: [] //ADD LIST OF LICENSES HERE
+    },
+    {
+        type: 'input',
+        message: 'Contributions',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
